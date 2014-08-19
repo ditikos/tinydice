b6f5aa059c3814e60b112e5c1c4990742f077991
Panagiotis Halatsakos MacHome <ditikos2@gmail.com>
2014-08-19 22:29:26 +0300

    updated now?

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require("socket.io").listen(server);
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 
