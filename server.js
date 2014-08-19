022b7d1817a1688437f2352dcca9fa18197d49b6
Panagiotis Halatsakos MacHome <ditikos2@gmail.com>
2014-08-19 21:25:52 +0300

    New Batch for openshift

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ io.on('connection', function(socket){
     usernum++;
 });
 
-http.listen(3000, function() {
-    console.log("Listening on port 3000");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+http.listen(server_port, server_ip_address, function() {
+    console.log("Listening on: http://"+server_ip_address+":"+server_port);
 });
